b63f6e0a694b38ad0b4ba237fc1167d4f994429f
greg.distler <greg.distler@gmail.com>
2014-08-08 18:54:53 -0500

    Update 2
    made changes to plot3.r and added the new figure

--- plot3.R
+++ plot3.R
@@ -6,3 +6,14 @@ hpcdata <- read.csv.sql("household_power_consumption.txt",sep = ";",sql = 'selec
 
 ## create a date/time column
 hpcdata$DateTime <- strptime(paste(hpcdata$Date,hpcdata$Time), "%d/%m/%Y %H:%M:%S")
+
+#make the plot - plot each sub_metering separately
+plot(hpcdata$DateTime,hpcdata$Sub_metering_1,type="l",ylab = "Energy sub metering")
+lines(hpcdata$DateTime,hpcdata$Sub_metering_2,col = "red")
+lines(hpcdata$DateTime,hpcdata$Sub_metering_3,col = "blue")
+#add a legend
+legend("topright",cex = .7,lty = 1,col = c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+## save plot
+dev.copy(png,file="plot3.png")
+dev.off()
